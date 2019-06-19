1025f9c0a2deee1bcf525d73f01415d012c617f8
Josh Johnson <jjmojojjmojo@gmail.com>
2019-06-19 15:21:14 -0400

    Increased the version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="random_quote",
-    version="0.1.0",
+    version="0.1.1",
     packages=['random_quote'],
     package_dir={'':'src'},
     install_requires=['webob'],
